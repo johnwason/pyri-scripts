b168def339664ba9fdcd4545e57b85b10014ade0
John Wason <wason@wasontech.com>
2021-03-15 02:49:59 -0400

    Add test_jog_tool.py

---
+++ test_jog_tool.py
@@ -0,0 +1,22 @@
+from pyri.device_manager_client import DeviceManagerClient
+from RobotRaconteur.Client import *
+import time
+
+d = DeviceManagerClient('rr+tcp://localhost:59902?service=device_manager', autoconnect=False)
+
+d.refresh_devices(1)
+
+d.connect_device('joint_jog')
+
+jog_service = d.get_device_client("joint_jog",1)
+
+print(jog_service.device_info.device.name)
+
+tool = jog_service.get_tool("tool")
+
+while(True):
+    tool.open()
+    time.sleep(0.5)
+    tool.close()
+    time.sleep(0.5)
+
