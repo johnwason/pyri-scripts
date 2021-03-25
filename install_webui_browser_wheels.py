47893dbdfc432726d46a7fdc4fde0660478bc3c8
John Wason <wason@wasontech.com>
2021-03-25 00:10:52 -0400

    Add PyPi wheel downloading to install_webui_browser_wheels.py

--- install_webui_browser_wheels.py
+++ install_webui_browser_wheels.py
@@ -1,11 +1,21 @@
+# Utility script to install WebUI Python wheels
+# These wheels are automatically downloaded by the browser and installed in the Pyodide virtual filesystem
+
+# Package names to build from the PyRI development tree
+wheels_names = ['pyri-webui-browser', 'pyri-common']
+# Package names to install from PyPi
+pip_wheels_names = ['importlib_resources','RobotRaconteurCompanion']
+
 import subprocess
 import re
 import glob
 import shutil
 import appdirs
 from pathlib import Path
+import requests
+from packaging.version import parse
 
-wheels_names = ['pyri-webui-browser', 'pyri-common']
+wheels_dir = Path(appdirs.user_data_dir(appname="pyri-webui-server", appauthor="pyri-project", roaming=False)).joinpath("wheels")
 
 def install_wheel(wheel_name):
     print(f"Installing {wheel_name}")
@@ -25,13 +35,56 @@ def install_wheel(wheel_name):
     wheel_name2 = wheel_name.replace("-","_")
 
     wheel_fname = Path(glob.glob(f"../{wheel_name}/dist/{wheel_name2}-{version}*.whl")[0], )
-    wheels_dir = Path(appdirs.user_data_dir(appname="pyri-webui-server", appauthor="pyri-project", roaming=False)).joinpath("wheels").joinpath(wheel_fname.name)
-    shutil.copyfile(wheel_fname,wheels_dir)
+    wheel_target_fname = wheels_dir.joinpath(wheel_fname.name)
+    shutil.copyfile(wheel_fname,wheel_target_fname)
     print(f"Copied wheel version {version} to {str(wheels_dir)}")
+
+def install_pip_wheel(wheel_name):
+    package_json = requests.get(f'https://pypi.python.org/pypi/{wheel_name}/json').json()
+    releases = package_json["releases"]
+    version = parse('0')
+    for r in releases:
+        ver = parse(r)
+        if ver > version:
+            version = ver
+
+    release = package_json["releases"][r][0]
+    fname = release["filename"]
+    assert fname.endswith(".whl")
+    url = release["url"]
+
+    wheel_target_fname = wheels_dir.joinpath(fname)
+    
+    existing_wheels = list(wheels_dir.glob(f"{wheel_name}*.whl"))
+    assert len(existing_wheels) <=1, "Multiple wheels of same package detected!"
+    if len(existing_wheels) > 0:
+        w = existing_wheels[0]
+        w_ver_res = re.match(f"{wheel_name}\\-([^\\-]+)-",w.name)
+        w_ver = parse(w_ver_res.group(1))
+        print(f"Existing package {wheel_name} is version {w_ver}")
+        if (w_ver >= version):
+            print(f"Existing package {wheel_name} is version {w_ver} is greater or equal to pypi, not replacing")
+            return
+        else:
+            print(f"Existing package {wheel_name} is version {w_ver} is older than pypi, replacing with version {version} from url {url}")
+            w.unlink()
+
+    print(f"Downloading and installing {wheel_name} package {version} from url {url}")
+
+    r = requests.get(url, allow_redirects=True)
+    open(wheel_target_fname, 'wb').write(r.content)
+
+
+
+
+    
 
 def main():
     for w in wheels_names:
         install_wheel(w)
+
+    for w in pip_wheels_names:
+        install_pip_wheel(w)
 
 if __name__ == "__main__":
     main()
