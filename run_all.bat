e204c436b5c3a1b185991b353279ccd05999f10a
John Wason <wason@wasontech.com>
2021-04-07 13:30:49 -0400

    Update to robotics_jog service

--- run_all.bat
+++ run_all.bat
@@ -5,5 +5,4 @@ wt new-tab -d . run_variable_storage.bat ;^
    new-tab -d . run_devices_states.bat ;^
    new-tab -d . run_sandbox.bat ;^
    new-tab -d . run_webui_server.bat ;^
-   new-tab -d . run_jog_joint.bat ;^
-   new-tab -d . run_jog_cart.bat
+   new-tab -d . run_robotics_jog.bat
