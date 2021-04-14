5703a3aa220ae542b081e8c5ae29048aecf11d9e
John Wason <wason@wasontech.com>
2021-04-14 14:23:43 -0400

    Add vision to add_default_devices

--- add_default_devices.bat
+++ add_default_devices.bat
@@ -6,3 +6,5 @@ setlocal
 ..\venv\Scripts\python add_device.py pyri_devices_states devices_states
 ..\venv\Scripts\python add_device.py pyri_sandbox sandbox
 ..\venv\Scripts\python add_device.py pyri_robotics_jog_service robotics_jog
+..\venv\Scripts\python add_device.py pyri_camera_viewer_service vision_camera_viewer
+..\venv\Scripts\python add_device.py pyri_camera_calibration_service vision_camera_calibration
